864edb3be36c29174417300a3cc8213766f645d2
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 17:13:00 -0500

    implemented cool methods from the command line

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,22 +6,31 @@ class Task < ActiveRecord::Base
   #   save
   # end
 
-  # def Task.edit(id, new_title)
-  #   task = Task.find(id)
-  #   task.title = new_title
-  #   task.save
-  # end
+  def Task.edit(id, new_title)
+    task = Task.find(id)
+    task.title = new_title
+    task.save
+  end
 
-  # def Task.complete(id)
-  #   task = Task.find(id)
-  #   task.completed = 1
-  #   task.completed_at = Time.now
-  #   task.save
-  # end
+  def Task.complete(id)
+    task = Task.find(id)
+    task.completed = 1
+    task.completed_at = Time.now
+    task.save
+  end
 
-  # def Task.list
-  #   Task.all.each{ |x| puts "#{x.id}. #{x.title}"}
-  # end
+  def Task.list
+    
+    Task.all.each do |x| 
+      if x.completed
+        c = "X"
+      else
+        c = " "
+      end
+      puts "#{x.id}. [#{c}] #{x.title}"
+    end
+
+  end
 end
 
 
--- todo.rb
+++ todo.rb
@@ -1,5 +1,16 @@
 require_relative 'config/application'
-
+require_relative 'app/models/task.rb'
 # puts "Put your application code in #{File.expand_path(__FILE__)}"
 
-Task.list
+if ARGV.any?
+
+  if ARGV[0] == "list"
+    Task.list
+  elsif ARGV[0] == "add"
+    Task.create(title: ARGV[1..-1].join(" "))
+  elsif ARGV[0] == "delete"
+    Task.delete(ARGV[1])
+  elsif ARGV[0] == "completed"
+    Task.complete(ARGV[1])
+  end
+end
